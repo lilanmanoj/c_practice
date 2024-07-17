d59cbdbebc1e2475018852a167930f3b5bb2ca65
Lilan Manoj <lilanmanoj@gmail.com>
2024-07-17 23:33:10 +0530

    Fine-tunes and added images of output for day 1 exercises

--- src/WD1/ex20.c
+++ src/WD1/ex20.c
@@ -2,24 +2,10 @@
 
 int main() {
     int n = 10;
-    printf("n = %d\n\n", n);
-    
-    int a = n--;
-    printf("a = n-- = %d\n", a);
-    printf("n = %d\n\n", n);
 
-    int b = --n;
-    printf("b = --n = %d\n", b);
-    printf("n = %d\n\n", n);
+    n -= (n--) - (--n);
 
-    n = 10;
-    int c = a - b;
-    int d = (n--) - (--n);
-    printf("a - b = %d\n", c);
-    printf("(n--) - (--n) = %d\n\n", d);
-
-    n -= c;
-    printf ("n -= (n--) - (--n) = %d\n", n);
+    printf ("n = %d\n", n);
 
     return 0;
 }
